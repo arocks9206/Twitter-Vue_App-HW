8ae33e89c394d082f42fdf8b581fb1b0195716c2
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-04-21 21:25:24 +0100

    completed mvp & some extensions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Twitter</h1>
+    <h2>Total Likes: {{totalLikes}}</h2>
+    <form v-on:submit.prevent="saveUser">
+        <h3>Add A New User</h3>
+        <input placeholder="Name" type="text" v-model="newUser.name" />
+        <input placeholder="Handle" type="text" v-model="newUser.handle"/>
+        <input placeholder="Avatar URL" type="text" v-model="newUser.img"/>
+        <input placeholder="Tweet" type="text" v-model="newUser.tweet"/>
+        <input placeholder="Likes" type="number" v-model="newUser.likes"/>
+        <input type="submit" value="Add User"/>
+    </form>
+      <ul>
+        <tweet-list-item v-for="(tweet, index) in tweets" :key="index" :tweet="tweet"></tweet-list-item>
+      </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TweetListItem from './components/TweetListItem.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      tweets:[
+        {
+          id: 1,
+          name: 'James',
+          handle: '@jokerjames',
+          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+          tweet: "If you don't succeed, dust yourself off and try again.",
+          likes: 10,
+        },
+        {
+          id: 2,
+          name: 'Fatima',
+          handle: '@fantasticfatima',
+          img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+          tweet: 'Better late than never but never late is better.',
+          likes: 12,
+        },
+        {
+          id: 3,
+          name: 'Xin',
+          handle: '@xeroxin',
+          img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+          tweet: 'Beauty in the struggle, ugliness in the success.',
+          likes: 18,
+        }
+      ],
+      newUser: {
+        name: "",
+        handle: "",
+        img: "",
+        tweet: "",
+        likes: 0,
+      },
+
+    };
+  },
   components: {
-    HelloWorld
+    'tweet-list-item': TweetListItem
+  },
+  computed: {
+    totalLikes: function(){
+      return this.tweets.reduce((total, tweet) => tweet.likes, 0);
+    },
+  },
+  methods: {
+    saveUser: function(){
+        this.tweets.unshift(this.newUser);
+
+        this.newUser = {
+          name: "",
+          handle: "",
+          img: "",
+          tweet: "",
+          likes: 0,
+        };
+      }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+header {
+  background: #eee;
+  padding: 10px 20px;
+  font: Helvetica Neue;
+  color: black;
+}
+
+form, #saveUser {
+  background: #eee;
+  padding: 10px 20px 20px 20px;
+  margin-top: 40px;
+}
+
+section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.tweets {
+  background: #b2ebf2;
+  padding: 10px 20px;
+  margin: 20px 0;
+  width: 20%;
 }
 </style>
---
+++ src/components/TweetListItem.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+<div>
+  <p>{{tweet.name}}</p>
+  <p>{{tweet.handle}}</p>
+  <img :src="tweet.img " width='100' height= '100'>
+  <p>{{tweet.tweet}}</p>
+  <p>{{tweet.likes}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'tweet-list-item',
+  props: ['tweet']
+}
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
